aa087ab31999c916551ca92311dd900af9665221
Olivier Lévy <olevy80@gmail.com>
2025-05-09 03:19:15 +0200

    feat(rust): Add generators for remaining logic blocks (null, ternary, operation, negate)

--- generators/rust/logic.ts
+++ generators/rust/logic.ts
@@ -68,8 +68,59 @@ export function logic_compare(block: Block, generator: RustGenerator): [string, 
   return [code, order];
 }
 
+export function logic_operation(block: Block, generator: RustGenerator): [string, number] {
+  // Operations 'AND' and 'OR'.
+  const operator = (block.getFieldValue('OP') === 'AND') ? '&&' : '||';
+  const order = (operator === '&&') ? generator.ORDER_LOGICAL_AND :
+      generator.ORDER_LOGICAL_OR;
+  let argument0 = generator.valueToCode(block, 'A', order);
+  let argument1 = generator.valueToCode(block, 'B', order);
+  if (!argument0 && !argument1) {
+    // If both operands are missing, return a default value.
+    argument0 = 'false';
+    argument1 = 'false';
+  } else {
+    // If one operand is missing, replace it with a default value.
+    // Rust requires boolean operands for && and ||.
+    if (!argument0) {
+      argument0 = 'false';
+    }
+    if (!argument1) {
+      argument1 = 'false';
+    }
+  }
+  const code = argument0 + ' ' + operator + ' ' + argument1;
+  return [code, order];
+}
+
+export function logic_negate(block: Block, generator: RustGenerator): [string, number] {
+  // Negation.
+  const order = generator.ORDER_LOGICAL_NOT;
+  const argument0 = generator.valueToCode(block, 'BOOL', order) || 'true';
+  const code = '!' + argument0;
+  return [code, order];
+}
+
 export function logic_boolean(block: Block, generator: RustGenerator): [string, number] {
   // Boolean values true and false.
   const code = (block.getFieldValue('BOOL') === 'TRUE') ? 'true' : 'false';
   return [code, generator.ORDER_ATOMIC];
+}
+
+export function logic_null(block: Block, generator: RustGenerator): [string, number] {
+  // Null data type.
+  return ['None', generator.ORDER_ATOMIC];
+}
+
+export function logic_ternary(block: Block, generator: RustGenerator): [string, number] {
+  // Ternary operator.
+  const value_if = generator.valueToCode(block, 'IF',
+      generator.ORDER_CONDITIONAL) || 'false';
+  const value_then = generator.valueToCode(block, 'THEN',
+      generator.ORDER_CONDITIONAL) || '/* then value */';
+  const value_else = generator.valueToCode(block, 'ELSE',
+      generator.ORDER_CONDITIONAL) || '/* else value */';
+  // Rust's if/else is an expression.
+  const code = 'if ' + value_if + ' { ' + value_then + ' } else { ' + value_else + ' }';
+  return [code, generator.ORDER_CONDITIONAL];
 }
